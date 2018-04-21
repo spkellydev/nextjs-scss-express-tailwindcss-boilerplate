faf65efd257f3d67390073e7650d782b9af990a9
Sean Kelly <spkellydev@gmail.com>
2018-04-21 00:35:52 -0400

    added express, but broke sass after universal routes

--- next.config.js
+++ next.config.js
@@ -1,2 +1,8 @@
 const withSass = require("@zeit/next-sass");
 module.exports = withSass();
+
+const getRoutes = require("./server/routes");
+
+module.exports = {
+  exportPathMap: getRoutes
+};
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,9 @@ import "../scss/style.scss";
 import "../scss/pages/index.scss";
 
 const Index = () => (
-  <div>
+  <div className="bg-gray-lightest">
     <div className="flex flex-col">
-      <div className="min-h-screen bg-pattern bg-center bg-smoke-light border-t-6 border-tailwind-teal flex items-center justify-center leading-tight p-6 pb-16">
+      <div className="con">
         <p className="example mt-12 font-light text-3xl text-center">
           Hello Next.js with Tailwind CSS
         </p>
