7c2df82b068f671e83017434083c27ff36f3dc57
Sean Kelly <spkellydev@gmail.com>
2018-04-20 21:48:00 -0400

    sass features tested

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 import "../scss/style.scss";
+import "../scss/pages/index.scss";
 
-const Index = () => <h1 className="example">Hello</h1>;
+const Index = () => (
+  <div>
+    <h1 className="example">
+      Hello <small>smaller</small>
+    </h1>
+    <p className="test">hellow</p>
+  </div>
+);
 
 export default Index;
